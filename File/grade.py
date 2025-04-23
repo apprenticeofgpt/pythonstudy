4f456d4278271fe32fe3ee6193d156363883f4a3
apprenticeofgpt <maddux0782@gmail.com>
2025-04-24 08:18:45 +0900

    코딩문제제

---
+++ File/grade.py
@@ -0,0 +1,199 @@
+import random as r
+
+# grade_dic={"1학년":{"1반":{"1번":{"국어":[],"수학":,"영어":}}}}
+grade = {
+    "1학년": {
+        "1반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "2반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "3반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "4반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "5반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+    }
+    ,
+    "2학년": {
+        "1반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "2반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "3반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "4반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "5반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+    }
+    ,
+    "3학년": {
+        "1반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "2반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "3반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "4반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "5반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+    }
+    ,
+    "4학년": {
+        "1반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "2반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "3반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "4반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "5반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+    }
+    ,
+    "5학년": {
+        "1반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "2반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "3반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "4반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "5반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+    }
+    ,
+    "6학년": {
+        "1반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "2반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "3반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "4반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+        ,
+        "5반": {
+            "국어": [r.randint(1, 100) for i in range(10)],
+            "수학": [r.randint(1, 100) for i in range(10)],
+            "영어": [r.randint(1, 100) for i in range(10)],
+        }
+    }
+}
+
+print(grade['1학년']['1반'])
+
